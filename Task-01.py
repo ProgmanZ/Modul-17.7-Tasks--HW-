d9002409769f00b562308bfe460eb5008bce640c
ProgmanZ <dumphill@gmail.com>
2021-12-15 09:48:11 +0300

    Task 01 - Готово

---
+++ Task-01.py
@@ -0,0 +1,9 @@
+# Задача 1. Гласные буквы
+
+user_text = input('Введите текст: ')
+
+letter_list = 'аеёиоуыя'
+used_letters = [letter for letter in user_text if letter in letter_list]
+
+print('\nСписок гласных букв:', used_letters)
+print('Длина списка:', len(used_letters))
