a9ea0337dbad0cd7acb1de9e6bae7e58b9507ff6
ProgmanZ <dumphill@gmail.com>
2021-12-24 22:22:10 +0300

    Task 01 - Готово. Task 02 - Готово. Task 03 - Готово. Task 04 - Готово. Task 05 - Готово. Task 06 - Готово. Task 07 - Готово. Task 08 - Готово. Task 09 - Готово. Task 10 - Готово.

--- Task-10.py
+++ Task-10.py
@@ -1,16 +1,10 @@
 # Задача 10. Шифр Цезаря (сделайте по желанию)
-#
-# def cesar_code(let, i):
-#     i = dictionary.index(let) - len(dictionary) + i
-#     return dictionary[i]
-
 
 dictionary = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяabcdefghijklmnopqrstuwvxyz'
 user_letters = str(input('Введите сообщение: '))
 user_letters.lower()
 k = int(input('Введите сдвиг: '))
 
-# result = [cesar_code(letter, k) if (letter != ' ') else ' ' for letter in user_letters]
 result = [dictionary[dictionary.index(letter) - len(dictionary) + k] if (letter != ' ') else ' '
           for letter in user_letters]
 
